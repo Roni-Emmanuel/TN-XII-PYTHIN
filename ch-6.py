61601d4e5fc68153721f7b68fd2b5b650170dfd5
Roni-Emmanuel <74718809+Roni-Emmanuel@users.noreply.github.com>
2025-08-05 17:32:53 +0530

    Add files via upload

---
+++ ch-6.py
@@ -0,0 +1,15 @@
+#this is a example and a very important 3mark
+x=int(input("Enter any number"))
+for i in range(1,11):
+    print(i,"x",x,"=",x*i)
+#This is a very important 3and 5mark
+#this is a example and a very important 3mark
+x=int(input("Enter any number"))
+y=int(input("Enter any number"))
+z=int(input("Enter any number"))
+if(x>=y)and(y>=z):
+    print(x)
+elif(y>=z):
+    print(y)
+else:
+    print(z)
